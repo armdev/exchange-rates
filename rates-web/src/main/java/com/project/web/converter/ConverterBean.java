5aed3d2fe4fff1aa157bd7ebc99be8c181a9ccf4
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-05-15 11:39:14 +0400

    up pom

--- rates-web/src/main/java/com/project/web/converter/ConverterBean.java
+++ rates-web/src/main/java/com/project/web/converter/ConverterBean.java
@@ -1,6 +1,5 @@
 package com.project.web.converter;
 
-import com.project.entities.Currency;
 import com.project.web.handlers.SessionContext;
 
 import com.project.web.service.ApplicationManager;
@@ -16,12 +15,13 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  *
- * @author Home
+ * @author armdev
  */
 @ManagedBean(name = "rateConverterBean")
 @SessionScoped
@@ -36,61 +36,34 @@ public class ConverterBean implements Serializable {
     @ManagedProperty("#{i18n}")
     @Setter
     private ResourceBundle bundle = null;
-
     @ManagedProperty("#{sessionContext}")
     @Setter
     private SessionContext sessionContext = null;
-
     private FacesContext context = null;
     private ExternalContext externalContext = null;
-
-    private List<String> currencyList = new ArrayList<>();
-
+    private final List<String> currencyList = new ArrayList<>();
+    @Setter
+    @Getter
     private String currencyFirst;
+    @Setter
+    @Getter
     private String currencySecond;
 
     @PostConstruct
     public void init() {
         context = FacesContext.getCurrentInstance();
-        externalContext = context.getExternalContext();
-      getCurrencyList();
+        externalContext = context.getExternalContext();     
+        applicationManager.getDictionaryService().findCurrencyList().forEach((c) -> {
+            currencyList.add(c.getCurrency());
+        });
     }
 
     public List<String> getCurrencyList() {
-        LOG.info("LIST SIZE " + applicationManager.getDictionaryService().findCurrencyList().size());
-        applicationManager.getDictionaryService().findCurrencyList().forEach((c) -> {
-            currencyList.add(c.getCurrency());
-        });       
         return currencyList;
     }
 
-  
-
-    public String getCurrencyFirst() {
-        return currencyFirst;
-    }
-
-    public void setCurrencyFirst(String currencyFirst) {
-        this.currencyFirst = currencyFirst;
-    }
-
-    public String getCurrencySecond() {
-        return currencySecond;
-    }
-
-    public void setCurrencySecond(String currencySecond) {
-        this.currencySecond = currencySecond;
-    }
-
-  
-
     public void addMessage(FacesMessage message) {
         FacesContext.getCurrentInstance().addMessage(null, message);
-    }
-
-    public String chpass() {
-
-        return "settings";
     }
 
 }
