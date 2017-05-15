8e4ffdddf7dc42caae641873392a5d75b478dd39
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-05-15 10:31:57 +0400

    up pom

--- rates-backend/src/main/java/com/project/dao/DictionaryDAOImpl.java
+++ rates-backend/src/main/java/com/project/dao/DictionaryDAOImpl.java
@@ -2,7 +2,6 @@ package com.project.dao;
 
 import com.project.entities.Currency;
 import java.util.List;
-import org.hibernate.Query;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -24,14 +23,9 @@ public class DictionaryDAOImpl extends AbstractDao implements DictionaryDAO {
         LOG.info("DictionaryDAO called");
     }
 
-    @Override    
-    public List<Currency> findCurrencyList() {
-        // Query query = getSession().createQuery("select id, currency from Currency ORDER BY currency ASC");
-        List<Currency> list = getSession().createCriteria(Currency.class).list();
-  for(Currency c :list){
-            LOG.info("LIST getCurrency " + c.getCurrency());
-            System.out.println("LIST wwwwwwwwwwwwwwwww " + c.getCurrency());
-        }
+    @Override
+    public List<Currency> findCurrencyList() {        
+        List<Currency> list = getSession().createCriteria(Currency.class).list();      
         return list;
     }
 
