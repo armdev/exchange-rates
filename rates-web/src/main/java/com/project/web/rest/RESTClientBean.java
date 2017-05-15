1319b14cb37d2db47a1422390e72cd76a4af0ef6
ARMEN ARZUMANYAN <armen.arzumanyan.external@atos.net>
2017-05-15 11:49:40 +0400

    up pom

--- rates-web/src/main/java/com/project/web/rest/RESTClientBean.java
+++ rates-web/src/main/java/com/project/web/rest/RESTClientBean.java
@@ -1,6 +1,5 @@
 package com.project.web.rest;
 
-
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -18,39 +17,38 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 
-
 @ManagedBean(eager = true, name = "restClient")
 @ApplicationScoped
 public class RESTClientBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
-  
+
     private final String SERVICE_PATH = "http://apilayer.net/api/";
 
-  
+    private final String ACCESS_KEY = "f4446d2499d427eca4efee698b587c1e";
+
     public RESTClientBean() {
-    
+
     }
 
     @PostConstruct
     public void init() {
-     
+
     }
 
-    public List<String> getRates() {
+    public ResponseModel getLiveRates(String firstCurrency, String secondCurrency) {
         CloseableHttpClient CLIENT = HttpClients.createDefault();
-     
-     
+        ResponseModel model = new ResponseModel();
         try {
-            HttpGet request = new HttpGet(SERVICE_PATH + "live?access_key=f4446d2499d427eca4efee698b587c1e&currencies=USD,EUR&format=1");
+            HttpGet request = new HttpGet(SERVICE_PATH + "live?ccess_key=" + ACCESS_KEY + "&currencies=" + firstCurrency + "," + secondCurrency + "&format=1");
             request.addHeader("charset", "UTF-8");
             HttpResponse response = CLIENT.execute(request);
             response.addHeader("content-type", "application/json;charset=UTF-8");
             HttpEntity entity = response.getEntity();
             ObjectMapper mapper = new ObjectMapper();
-           
-            ResponseModel model = mapper.readValue(EntityUtils.toString(entity), ResponseModel.class);
-          System.out.println(model.toString());
+
+            model = mapper.readValue(EntityUtils.toString(entity), ResponseModel.class);
+            System.out.println(model.toString());
         } catch (IOException | ParseException ex) {
             try {
                 CLIENT.close();
@@ -64,9 +62,7 @@ public class RESTClientBean implements Serializable {
                 java.util.logging.Logger.getLogger(RESTClientBean.class.getName()).log(Level.SEVERE, null, ex1);
             }
         }
-        return null;
+        return model;
     }
-
-    
 
 }
