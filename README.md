52016b1803fa2b60a4ccba404fcccd8f85fb4428
armdev <armen.arzumanyan@gmail.com>
2017-05-18 01:36:16 +0400

    up

---
+++ README.md
@@ -0,0 +1,14 @@
+Exchange Rates
+
+Simple Java Web Application
+
+Technologies used:
+
+1. Spring 4
+2. Hibernate 4
+3. JSF 2.2
+4. Primefaces 6.1
+5. Apache Http Client
+6. MySQL
+7. H2 in memory database.
+8. Lombok
--- db/rates_db.sql
+++ db/rates_db.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.50 
-MySQL - 5.5.5-10.1.13-MariaDB : Database - rates
+MySQL - 5.0.51b-community-nt : Database - rates
 *********************************************************************
 */
 
@@ -21,31 +21,31 @@ USE `rates`;
 DROP TABLE IF EXISTS `currency`;
 
 CREATE TABLE `currency` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `currency` varchar(3) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id` bigint(11) NOT NULL auto_increment,
+  `currency` varchar(3) default NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 /*Data for the table `currency` */
 
-insert  into `currency`(`id`,`currency`) values (1,'EUR'),(2,'USD'),(3,'AMD'),(4,'CBP'),(5,'NZD'),(6,'AUD'),(7,'JPY'),(8,'HUF'),(9,'ZWD'),(10,'CAD');
+insert  into `currency`(`id`,`currency`) values (1,'EUR'),(2,'AMD'),(3,'NZD'),(4,'AUD'),(5,'JPY'),(6,'HUF'),(7,'CAD');
 
 /*Table structure for table `historical` */
 
 DROP TABLE IF EXISTS `historical`;
 
 CREATE TABLE `historical` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `usdeur` double DEFAULT NULL,
-  `usdamd` double DEFAULT NULL,
-  `usdnzd` double DEFAULT NULL,
-  `usdjpy` double DEFAULT NULL,
-  `usdhuf` double DEFAULT NULL,
-  `usdcad` double DEFAULT NULL,
-  `usdaud` double DEFAULT NULL,
-  `historical_date` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` bigint(11) NOT NULL auto_increment,
+  `usdeur` double default NULL,
+  `usdamd` double default NULL,
+  `usdnzd` double default NULL,
+  `usdjpy` double default NULL,
+  `usdhuf` double default NULL,
+  `usdcad` double default NULL,
+  `usdaud` double default NULL,
+  `historical_date` varchar(20) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `historical` */
 
@@ -54,24 +54,24 @@ CREATE TABLE `historical` (
 DROP TABLE IF EXISTS `user`;
 
 CREATE TABLE `user` (
-  `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `passwd` varchar(100) DEFAULT NULL,
-  `register_date` datetime DEFAULT NULL,
-  `birth_date` datetime DEFAULT NULL,
-  `last_login_date` datetime DEFAULT NULL,
-  `country` varchar(50) DEFAULT NULL,
-  `city` varchar(50) DEFAULT NULL,
-  `street` varchar(50) DEFAULT NULL,
-  `zipcode` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `id` bigint(11) NOT NULL auto_increment,
+  `firstname` varchar(100) default NULL,
+  `lastname` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `passwd` varchar(100) default NULL,
+  `register_date` datetime default NULL,
+  `birth_date` datetime default NULL,
+  `last_login_date` datetime default NULL,
+  `country` varchar(50) default NULL,
+  `city` varchar(50) default NULL,
+  `street` varchar(50) default NULL,
+  `zipcode` varchar(50) default NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`id`,`firstname`,`lastname`,`email`,`passwd`,`register_date`,`birth_date`,`last_login_date`,`country`,`city`,`street`,`zipcode`) values (11,'John','Smith','armen@gmail.com','0b4e7a0e5fe84ad35fb5f95b9ceeac79','2017-05-13 21:55:33',NULL,NULL,'Armenia','Yerevan','new streeet','0295595');
+insert  into `user`(`id`,`firstname`,`lastname`,`email`,`passwd`,`register_date`,`birth_date`,`last_login_date`,`country`,`city`,`street`,`zipcode`) values (11,'John','Smith','don@gmail.com','0b4e7a0e5fe84ad35fb5f95b9ceeac79','2017-05-13 21:55:33',NULL,NULL,'Armenia','Yerevan','new streeet','0295595');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
